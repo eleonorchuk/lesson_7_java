9fd50d343f041c90104b3f8be4c2ef8cc2590dcf
eleonorchuk <zoimy@ukr.net>
2021-09-28 22:50:26 +0300

    taask_2

--- src/ru/geekbrains/lessons/Cat.java
+++ src/ru/geekbrains/lessons/Cat.java
@@ -3,21 +3,26 @@ package ru.geekbrains.lessons;
 public class Cat {
     private String name;
     private int appetite;
+    private boolean fullness;
+
     public Cat(String name, int appetite) {
         this.name = name;
         this.appetite = appetite;
+        this.fullness = false;
         System.out.println("cate create: " + name + " " + appetite);
     }
+
     public void eat(Plate p) {
         System.out.println("cate eat: " + name);
         if (p.decreaseFood(appetite)) {
+            this.fullness = true;
             System.out.println(appetite + " food");
         }
         else
         {
             System.out.println("not enough food in plate");
         }
-
+        System.out.println("cat " + name +" fullness = " + this.fullness);
     }
 
     public int getAppetite() {
